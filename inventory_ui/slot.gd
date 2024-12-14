c35679c508f385380c22385e2034dc5407a3fe4e
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2024-12-14 23:11:00 +0100

    finishes drag and drop

--- inventory_ui/slot.gd
+++ inventory_ui/slot.gd
@@ -5,80 +5,93 @@ extends PanelContainer
 @export var resource_data: PickupInventoryResource
 #resource slot will save the SLOT data of object that was picked up
 @export var resource_slot: SlotData
+
+#node references
 @onready var texture_rect: TextureRect = $MarginContainer/TextureRect
 @onready var quantity_label: Label = $Label
 
 
-func _ready() -> void:
-	#if resource:
-		#texture_rect.texture = resource.texture
-	pass
-
+#region Setup Slot when first created
 func set_slot_data(slot_data: SlotData):
 	resource_data = slot_data.item_data
 	resource_slot = slot_data
-	texture_rect.texture = resource_data.texture
-	tooltip_text = "%s\n%s" % [resource_data.name, resource_data.description]
+	updata_slot_display()
 	
-	if resource_slot.quantity > 1:
-		quantity_label.text = "x%s" % resource_slot.quantity
-		quantity_label.show()
-	else:
-		quantity_label.hide() 
-	
+#endregion
 	
 
 #region Drag and Drop Logic
 
 func _get_drag_data(at_position: Vector2) -> Variant:
-	print("You got me")
 	if resource_data:
-		var prev = TextureRect.new()
-		prev.texture = resource_data.texture
-		set_drag_preview(prev)
-		return {"resource_data": resource_data, "resource_slot": resource_slot, "source_node": self}
+		var drag_preview = TextureRect.new()
+		drag_preview.texture = resource_data.texture
+		set_drag_preview(drag_preview)
+		return {
+			"resource_data": resource_data, 
+			"resource_slot": resource_slot, 
+			"source_node": self
+			}
 	return null
 #
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	return true
-	return false
+	return data.has("resource_data") and data.has("resource_slot")
 #
 func _drop_data(at_position: Vector2, data: Variant) -> void:
-	var reset_slot: bool = true
-	var resource_data_dropped = data["resource_data"]
-	var resource_slot_dropped = data["resource_slot"]
-	var source_node = data["source_node"]
+	if data.has("resource_data") and data.has("resource_slot"):
+		var source_node = data.get("source_node")
+		swap_or_assign_resources(data["resource_data"], data["resource_slot"], source_node)
 	
-#	swap data
-	if resource_data and resource_slot:
-		reset_slot = false
-		source_node.resource_data = resource_data
-		source_node.resource_slot = resource_slot
-		source_node.update_if_swap()
-	
-	resource_data = resource_data_dropped
-	resource_slot = resource_slot_dropped
-	texture_rect.texture = resource_data.texture
+# Handle swapping or assigning resources during a drop
+func swap_or_assign_resources(new_data: PickupInventoryResource, new_slot: SlotData, source_node: Node) -> void:
+	var reset_source = true
 
-	if source_node and source_node != self and reset_slot:
-		source_node.clear_resource()
+	# Swap resources if this slot already has data
+	if resource_data and resource_slot:
+		reset_source = false
+		source_node.assign_resource(resource_data, resource_slot)
 	
+	# Assign new resources to this slot
+	assign_resource(new_data, new_slot)
+
+	# Clear the source slot if necessary
+	if source_node and source_node != self and reset_source:
+		source_node.clear_resource()
+
+# Assign resources to the slot and update the display
+func assign_resource(new_data: PickupInventoryResource, new_slot: SlotData) -> void:
+	resource_data = new_data
+	resource_slot = new_slot
+	updata_slot_display()
+
+
+
+#endregion
+#region helper functions
+
+func updata_slot_display() -> void:
+	if resource_data:
+		texture_rect.texture = resource_data.texture
+		tooltip_text = "%s\n%s" % [resource_data.name, resource_data.description]
+	else:
+		texture_rect.texture = null
+		tooltip_text = ""
+
+	if resource_slot and resource_slot.quantity > 1:
+		quantity_label.text = "x%s" % resource_slot.quantity
+		quantity_label.show()
+	else:
+		quantity_label.hide()
 
 func clear_resource() -> void:
-	print("Reset")
 	resource_data = null
 	resource_slot = null
-	texture_rect.texture = null
-	quantity_label = null
+	updata_slot_display()
+	#texture_rect.texture = null
+	#quantity_label = null
 
 func update_if_swap():
 	texture_rect.texture = resource_data.texture
-	
-
-
-
-#func swap_resources(origin_slot) -> void:
-	#origin_slot.resource = resource
-	#origin_slot.texture_rect.texture = texture_rect.texture
-
+	quantity_label.text = "x%s" % resource_slot.quantity
+	quantity_label.show()
 #endregion
